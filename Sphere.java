83c3197298a0f6e20149cce6df37e826f481eb8a
Robert McManus <rmcmanus@bu.edu>
2015-03-26 15:06:54 -0400

    Create Sphere.java

---
+++ Sphere.java
@@ -0,0 +1,15 @@
+/*
+ * Robert Conner McManus
+ * PA4
+ * 12/3/14
+ * 
+ * Sphere.java
+ * defines functions to draw a sphere to the screen
+*/
+
+public class Sphere extends Ellipsoid{
+
+	public Sphere(float radius, SketchBase sketch){
+		super(radius, radius, radius, sketch);
+	}
+}
